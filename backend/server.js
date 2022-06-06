1c55b839b003c719b63e68e77b935e5c429cff32
pavan <pavangurram56@gmail.com>
2022-06-06 23:14:38 +0530

    server intialized

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("server.");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "welcome to E-Learn" });
+});
+
+app.listen(PORT, () => console.log(`server started on port ${PORT} 🎉🎉`));
