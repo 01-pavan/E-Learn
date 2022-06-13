b384a8b4fe356af97d7ac6d1b29ec304e8378758
pavan <pavangurram56@gmail.com>
2022-06-14 00:16:02 +0530

    controller for comments

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+
+const Comment = require("../models/commentModal");
+
+const createComment = asyncHandler(async (req, res) => {
+  const { courseId, userName, comment } = req.body;
+
+  const createdComment = await Comment.create({
+    courseId,
+    userName,
+    comment,
+  });
+
+  res.status(201).json(createdComment);
+});
+const getComments = asyncHandler(async (req, res) => {
+  const { course_id } = req.query;
+
+  const comments = await Comment.find({ courseId: course_id });
+  console.log(course_id);
+  res.status(200).json(comments);
+});
+
+module.exports = {
+  createComment,
+  getComments,
+};
