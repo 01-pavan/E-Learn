7e9933dd6e276f18f85b0b8af42b9ba888fd4947
pavan <pavangurram56@gmail.com>
2022-06-17 16:12:27 +0530

    buy now and goto course toggling functionality implemented

--- frontend/src/pages/CourseDetailsPage.jsx
+++ frontend/src/pages/CourseDetailsPage.jsx
@@ -4,12 +4,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { getCourse, reset } from "../features/courses/courseSlice";
 import { useParams, useNavigate } from "react-router-dom";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import InfoIcon from "@mui/icons-material/Info";
 import { createEnrolledCourse } from "../features/enrolledCourses/enrolledCourseSlice";
+import axios from "axios";
+import { useState } from "react";
 
 const CourseDetailsPage = () => {
   const { course, isLoading, message, isError, isSuccess } = useSelector(
     (state) => state.course
   );
+  const [isEnrolled, setIsEnrolled] = useState(false);
+  const [enrolledTimestamp, setIsEnrolledTimeStamp] = useState("");
+
   const { user } = useSelector((state) => state.auth);
 
   const { enrolledCourses } = useSelector((state) => state.enrolledCourse);
@@ -20,6 +26,31 @@ const CourseDetailsPage = () => {
   const dispatch = useDispatch();
   const { courseId } = useParams();
   const navigate = useNavigate();
+
+  //checking current course is enrolled or not
+  const checkIsEnrolled = () => {
+    enrolledCourses.forEach((item) => {
+      if (item.course._id === courseId) {
+        setIsEnrolled(true);
+        setIsEnrolledTimeStamp(item.createdAt);
+        console.log("enrolled🎉🎉🎉");
+      }
+      console.log("not enrolled🤨🤨🤨🤨");
+    });
+  };
+  useEffect(() => {
+    checkIsEnrolled();
+  }, [enrolledCourses]);
+
+  //formating mongoDb createdAt timestamp to date
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
 
   useEffect(() => {
     console.log("useeffect from course detai;s");
@@ -44,13 +75,19 @@ const CourseDetailsPage = () => {
     dispatch(createEnrolledCourse(enrolledCourseData));
   };
 
+  const checkout = async () => {
+    const res = await axios.post("http://localhost:5000/api/payment");
+
+    console.log(res);
+  };
+
   return (
     <>
       <Header />
       <div className="w-full mx-auto scrollbar-hide">
         <div className="bg-gradient-to-r from-zinc-900 via-zinc-800 to-zinc-900">
-          <div className="flex max-w-6xl px-5 w-full mx-auto pt-4">
-            <div className="flex-grow text-white relative">
+          <div className="flex  max-w-6xl px-5 w-full mx-auto pt-4">
+            <div className="flex-grow text-white  relative">
               <div className="max-w-xl py-6">
                 <h1 className="text-4xl font-bold">{course?.title}</h1>
                 <h2 className="py-4 text-base font-semibold">
@@ -71,24 +108,48 @@ const CourseDetailsPage = () => {
                     alt=""
                   />
                 </div>
+
                 <div className="p-6 text-black">
-                  <h1 className="text-3xl font-bold">
-                    ₹{course.price}{" "}
-                    <span className="text-base font-normal line-through	">
-                      3999
-                    </span>
-                  </h1>
-                  <button
-                    onClick={() => {
-                      postEnrolledCourse();
-                      // navigate("/learn");
-                    }}
-                    type="submit"
-                    // disabled
-                    className="w-full px-4 py-2 my-4 bg-indigo-600 rounded-md hover:bg-indigo-700 text-white"
-                  >
-                    Buy Now
-                  </button>
+                  {isEnrolled ? (
+                    <div className="flex justify-center space-x-2">
+                      <InfoIcon className="" />
+                      <h1 className="text-xl font-semibold">
+                        You purchased this course on{" "}
+                        {formatTimestamp(enrolledTimestamp)}
+                      </h1>
+                    </div>
+                  ) : (
+                    <h1 className="text-3xl font-bold">
+                      ₹{course.price}{" "}
+                      <span className="text-base font-normal line-through	">
+                        3999
+                      </span>
+                    </h1>
+                  )}
+
+                  {isEnrolled ? (
+                    <button
+                      onClick={() => {
+                        navigate("/learn");
+                      }}
+                      type="submit"
+                      className="w-full px-4 py-2 my-4 bg-indigo-600 rounded-md hover:bg-indigo-700 text-white"
+                    >
+                      Go to Course
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        postEnrolledCourse();
+                        checkout();
+                      }}
+                      type="submit"
+                      // disabled
+                      className="w-full px-4 py-2 my-4 bg-indigo-600 rounded-md hover:bg-indigo-700 text-white"
+                    >
+                      Buy Now
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
